14bd14bb3c23b0122a3bddadc1842d2238cab88c
Artsem Pauliushchyk <artsiom.p@customgamesstudio.com>
2026-03-25 09:34:58 +0300

    Add mey main menu
    # Conflicts:
    #	Assets/Scenes/SampleScene.unity
    #	Assets/Scripts/UI/MenuScript.cs

--- Assets/Scripts/Buttons/CloseBtn.cs
+++ Assets/Scripts/Buttons/CloseBtn.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class CloseBtn : BaseButton {
     protected override void OnClick() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
